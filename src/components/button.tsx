a5edf0f64449d147f34dc54daf7246bf73c5538c
Antonio Fonseca <antonio.fonse06@gmail.com>
2024-11-03 17:20:28 -0300

    Fix deploy error

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,20 @@
 import { motion, MotionProps } from "framer-motion"
 
-import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-interface ButtonProps extends MotionProps, ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends MotionProps {
   title: string
   icon?: ReactNode
   className?: string
+  onClick?: () => void
 }
 
-export function Button ({ title, icon, className, ...props }: ButtonProps) {
+export function Button ({ title, icon, className, onClick, ...props }: ButtonProps) {
   return (
     <motion.button
       className={`flex items-center justify-center mr-7 bg-blues-blue_900 text-whites-white_200 p-3 rounded-full gap-2 ${className}`}
       whileHover={{ scale: 1.10 }}
+      onClick={ onClick }
       { ...props }
     >
       <h1 className='lg:text-xl md:text-base sm:text-sm text-whites-white_200 font-semibold ' >
